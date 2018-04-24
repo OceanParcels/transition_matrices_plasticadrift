58e9df27de2139c423b4aca4abc6460bf1a6a7a8
jeroen1405 <31624947+jeroen1405@users.noreply.github.com>
2018-04-24 22:12:07 +0200

    Add files via upload

---
+++ frac_coast_model.py
@@ -0,0 +1,87 @@
+import random
+import sys
+import os
+import csv
+import numpy as np
+from numpy import genfromtxt
+
+import createrawmatrix
+
+class structtype():
+    pass
+
+M = createrawmatrix.M
+
+region = 'global'
+
+earthGrid = genfromtxt(os.path.join(os.getcwd(),'elevationdata.csv'), delimiter=',',dtype=float)
+earthGrid = np.array(earthGrid)
+#earthGrid is a 3601x1801 matrix, where the first column is latitude (89.95 up until -89.95, from North to South with increasing index)
+#and the first row being latitude from -179.95 up to 179.95
+
+lats = earthGrid[1::,0]
+lons = earthGrid[0,1::]
+
+#strip grid of index column and row and rearrange the matrix to fit index order of createrawmatrix (and other files)
+earthGrid = np.delete(earthGrid,0,1) #delete latitude index column
+shift = np.where(earthGrid[0] > 0)[0][0]
+earthGrid = np.roll(earthGrid,shift,axis=1) #shift matrix from -180 < lon < 180 to 0 < lon < 360
+earthGrid = np.delete(earthGrid,0,0) #delete longitude index row
+earthGrid = np.flipud(earthGrid) #reverse matrix from 90 > lon > -90 to -90 < lon < 90
+
+#rearrange index order
+lons = np.roll(lons,shift)
+lats = np.fliplr([lats])[0]
+
+lons[np.where(lons < 0)] += 360
+
+coastalGrid = np.empty(np.shape(earthGrid),dtype=int)
+
+for row in np.arange(lats.size):
+    for col in np.arange(lons.size):
+        if earthGrid[row][col] > 9998: #sea
+            coastalGrid[row][col] = 0              
+        else: #land
+            coastalGrid[row][col] = 1
+
+#convert input grid to flat indices
+inputMesh = np.meshgrid(lons,lats)
+
+nx = inputMesh[0].flatten()
+ny = inputMesh[1].flatten()
+coastalGrid = coastalGrid.flatten()
+
+ni = np.empty(coastalGrid.shape,dtype='int')
+ni[:] = -9999
+
+#compare coastalGrid to createrawmatrix resolution 
+for d in np.arange(M.nd):
+    D = np.where((nx >= M.lons[d][0]) & (nx <= M.lons[d][-1]) & (ny >= M.lats[d][0]) & (ny <= M.lats[d][-1]))[0] #take data lons and lats between defined bounds of Model lons and lats.
+    xi = np.floor( (nx[D]-M.lons[d][0])/M.dd[d] ).astype(int) #iterative start values for lon/lat within area are data values lon/lat minus first lon/lat values divided by stepsize
+    yi = np.floor( (ny[D]-M.lats[d][0])/M.dd[d] ).astype(int)  #see above. If lon of data and lon of model target match, xi[0] = 0, otherwise e.g. lon.data = 4, lon.target = 1, 5 samples per 1 lat -> xi starts at ((4-1)/0.2) = 15. Note: xi/yi is all eligible data values, not just start value
+
+    for i in np.arange(xi.size):
+        ni[D[i]] =  np.ravel_multi_index((xi[i],yi[i]),(M.nx[d],M.ny[d]),order='F') #similarly to method in createrawmatrix
+
+    resultIndex = np.arange(0,M.nc[d]) #indexation identical to model (createrawmatrix)
+    resultCoast = np.zeros(M.nc[d]) #array for summing land/sea/coast data per index
+    resultNorm = np.zeros(M.nc[d]) #amount of cells from input grid per cell for output grid
+    
+    for i in np.arange(coastalGrid.size) :
+        if ni[i] > -9900:
+            if coastalGrid[i] == 1 :
+                resultCoast[ni[i]] += coastalGrid[i]
+                resultNorm[ni[i]] += 1
+            else :
+                resultNorm[ni[i]] += 1
+
+    resultNorm[np.where(resultNorm == 0)] = 100 #edge indices (prime meridian, poles) don't have coastalGrid cells in them (so will remain 0) but also do not alter results. This statements prevents division by zero
+    resultCoast = resultCoast / resultNorm
+
+    f = open("frac_coast_dx_" + str((M.dd[d] * 100).astype(int)) + "_region_" + region + ".csv",'w') #also clear it if it exists
+    f.write('index,lat,lon,fraction\n')
+
+    for i in np.arange(resultIndex.size) : #write land/sea fraction to file for every location
+        x, y = np.unravel_index(resultIndex[i],(M.nx[d],M.ny[d]),order='F')
+        f.write(str(resultIndex[i]) + ',' + str(M.lats[d][y]) + ',' + str(M.lons[d][x]) + ',' + str(resultCoast[i]) + '\n')
+    f.close()
---
+++ global_fwd_csv.py
@@ -0,0 +1,124 @@
+import random
+import sys
+import os, os.path
+os.environ["TF_CPP_MIN_LOG_LEVEL"]="3"
+import csv
+import numpy as np
+from scipy import sparse
+from numpy import genfromtxt
+import calendar
+
+import createrawmatrix
+
+class structtype():
+    pass
+
+def ravel(lat, lon) : #function for conversion between latitudes and longitudes (user friendly) to index of model
+    ind = np.ravel_multi_index((lon-M.lons[-1][0],lat-M.lats[-1][0]),(M.nx[-1],M.ny[-1]),order='F')
+    return ind
+
+region = 'global'
+inputFolder = 'TransitMatrices'
+outputFolder = 'CSV_files'
+
+beaching = True #whether to include beaching into results or not
+beachingmodel = 'frac' #other option: 'adj'. Set beaching model to either the Fractional Coast Model or Adjacent Coast Model, see paper.
+beachingpercentage = 30 #scaling factor for beaching model, in procent
+
+M = createrawmatrix.M #import createrawmatrix for parameters of model (such as range longitudes, latitudes, dx, dt)
+landpoints = genfromtxt('Landpoints/landpoints.csv', delimiter=',') #array of 1's for land and 0's for sea for every grid cell in model
+
+maxyears = 10 #duration of simulation
+minplotval = 0.000000000001 #minimal value included in results. If some location has less tracer in it than this amount, the tracer is removed from the ocean.
+offset = np.arange(M.nt) #array of all starting timesteps that are evaluated
+numints = maxyears*M.nt + 1 #number of computational iterations
+
+#release origins to be evaluated. Input is in indices of model, but can be in latitude and longitude with ravel()
+indices = ravel(np.array([51,52,53,53,53]) , np.array([3, 4, 5, 6, 7])) #indices for Dutch coast
+
+#rename target area(s) from createrawmatrix for brevity
+lon = np.array(M.lons[-1])
+lat = np.array(M.lats[-1])
+
+transitMatrix = [[] for x in range(M.nt)]
+for x in range(M.nt):
+    transitMatrix[x] = sparse.load_npz(os.path.join(inputFolder, 'hybridtransitmatrix_'+M.dir+'_month_'+str(x)+'.npz'))
+
+if beaching: #load beaching data corresponding to set model and determine the chance of particle tracer ending on shore
+    beachdata = np.genfromtxt(os.path.join('beachingCoastlines', beachingmodel + '_coast_dx_' + str((M.dd[-1] * 100).astype(int)) + '_region_'+region+'.csv'),delimiter=',',skip_header=1)
+    beachdata = np.transpose(beachdata)
+    beach = 1 - beachdata[3] * beachingpercentage / 100 #chance of tracer ending on shore
+
+    if beach.size != M.nc[-1] :
+        print("Resolution of beaching grid and transition matrix don't match")
+        sys.exit()
+
+for i in indices:
+    if landpoints[i] == 0 :
+        folder = outputFolder #reset the output folder
+    
+        if beaching: #create subfolder within index folder to differentiate beaching scaling sizes 
+            try :
+                os.mkdir(os.path.join(folder, "beach_" + str(beachingpercentage) + "_perc"))
+                folder = folder + "//beach_" + str(beachingpercentage) + "_perc"
+            except:
+                folder = folder + "//beach_" + str(beachingpercentage) + "_perc"
+        else:
+            try : #create subfolder per index, unless it already exists
+                os.mkdir(os.path.join(folder, "no_beaching"))
+                folder = folder + "//no_beaching"
+            except:
+                folder = folder + "//no_beaching"
+
+        for t in offset :
+            m = int(np.floor(t * 12/M.nt)) #calendar month on which the iteration starts (first day of month m)
+
+            if beaching : #save tracer per location per timestep in "savefile" and save the amount of tracer ending on shore per location per timestep in "totalbeachedfile"
+                savefile = os.path.join(folder, str(i) + "_startsin" + calendar.month_name[m+1] + "_beaching_" + str(beachingpercentage) + ".csv") #m+1 because month_index 0 is month 1 (January)
+                totalbeachedfile = os.path.join(folder, str(i) + "_startsin" + calendar.month_name[m+1] + "_beaching_" + str(beachingpercentage) + "totalbeached.csv")
+            else: #save tracer per location per timestep in "savefile"
+                savefile = os.path.join(folder, str(i) + "_startsin" + calendar.month_name[m+1] + ".csv") #m+1 because month_index 0 is month 1 (January)
+            f1 = open(savefile,'w')
+            f1.write('year,month,lat,lon,prob\n') #save index header
+            if beaching:
+                f2 = open(totalbeachedfile,'w')
+                f2.write('year,month,lat,lon,beachedtracer\n') #save index header
+
+            v = np.zeros(M.nc[-1])
+            v[i] = 1 #set release location to 1, all other locations 0 for first iteration.
+            count = 0 #counter denoting current iteration
+
+            for n in np.arange(numints) + t :
+                bm = n % M.nt #timestep of current iteration, modulated by number of timesteps to account for offset t
+                mon = np.floor((count % M.nt) * 12/M.nt).astype(int) #calendar month of current timestep
+                year = np.floor(count/M.nt).astype(int) #year of current timestep
+
+                #The vector-matrix multiplication
+                nums, =np.where(v > minplotval)
+                pX, pY = np.unravel_index(nums,(M.nx[-1],M.ny[-1]),order='F') #convert index to geospatial coordinates
+
+                vals = v[nums] #save all tracer amounts > minplotval to file f1
+                for j in np.arange(vals.size) :
+                    val = vals[j]
+                    f1.write(str(year) + ',' + str(mon) + ',' + str(lat[pY[j]]) + ',' + str(lon[pX[j]]) + ',' + str(val) + '\n')
+                count += 1
+                v = v * transitMatrix[bm] #multiply v with transit matrix for next iteration
+
+
+                if beaching:
+                    initialv = v
+                    v = v * beach #multiply v with chance of particles beaching at that location
+                    diffv = initialv - v 
+
+                    nums, =np.where(diffv > minplotval)
+                    pX, pY = np.unravel_index(nums,(M.nx[-1],M.ny[-1]),order='F')
+
+                    vals = diffv[nums]
+                    for j in np.arange(pX.size) : #save the total amount of beached tracer per location to file f2
+                        val = vals[j]
+                        f2.write(str(year) + ',' + str(mon) + ',' + str(lat[pY[j]]) + ',' + str(lon[pX[j]]) + ',' + str(val) + '\n')
+            f1.close()
+            if beaching:
+                f2.close()
+    else:
+        print('Index ' + str(i) + ' is a point on land.')
